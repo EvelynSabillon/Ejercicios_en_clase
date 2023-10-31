cb0e75de6316f6b605de0b1a70979f544f827a75
EvelynSabillon <145521401+EvelynSabillon@users.noreply.github.com>
2023-10-31 14:02:41 -0600

    cambios2

---
+++ tarea_02_evelyn_sabillon/lib/contact_info.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class ContactInfo extends StatelessWidget {
+  const ContactInfo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar( 
+        title: const Text('Contacto'), 
+      ),
+    );
+  }
+}
--- tarea_02_evelyn_sabillon/lib/main.dart
+++ tarea_02_evelyn_sabillon/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tarea_02_evelyn_sabillon/contact_info.dart';
 
 void main() => runApp(const MyApp());
 
@@ -8,15 +9,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
+      debugShowCheckedModeBanner: false,
+      title: 'Contactos',
+      theme: ThemeData(
+        useMaterial3: true,
+        primarySwatch: Colors.green,
       ),
+      initialRoute: '/contactInfo',
+      routes: {
+        '/contactInfo':(context) => const ContactInfo()
+      },
     );
   }
 }
