9af0c4d962f783a93917780fc209924cefd6cf4f
EvelynSabillon <145521401+EvelynSabillon@users.noreply.github.com>
2023-10-31 15:29:07 -0600

    Personalizando la informacion de contacto

--- tarea_02_evelyn_sabillon/lib/contact_info.dart
+++ tarea_02_evelyn_sabillon/lib/contact_info.dart
@@ -1,14 +1,42 @@
 import 'package:flutter/material.dart';
 
-
 class ContactInfo extends StatelessWidget {
   const ContactInfo({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar( 
-        title: const Text('Contacto'), 
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.arrow_back),
+        ),
+      ),
+      body: const Padding(
+        padding: EdgeInsets.all(16),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                backgroundColor: Color.fromARGB(255, 244, 54, 133),
+                radius: 50,
+                child: Text("P",
+                    style: TextStyle(fontSize: 50, color: Colors.white)),
+              ),
+              SizedBox(height: 40),
+              Text(
+                "Pascualillo",
+                style: TextStyle(fontSize: 24),
+              ),
+
+              Row(
+
+              ),
+
+            ],
+          ),
+        ),
       ),
     );
   }
