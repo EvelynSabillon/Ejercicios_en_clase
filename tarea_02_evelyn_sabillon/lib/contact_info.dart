218cdc12f62ee55629154b442d58c88edb51efe9
EvelynSabillon <145521401+EvelynSabillon@users.noreply.github.com>
2023-11-03 23:39:44 -0600

    Agregar botones de llamada mensaje y video

--- tarea_02_evelyn_sabillon/lib/contact_info.dart
+++ tarea_02_evelyn_sabillon/lib/contact_info.dart
@@ -12,28 +12,73 @@ class ContactInfo extends StatelessWidget {
           icon: const Icon(Icons.arrow_back),
         ),
       ),
-      body: const Padding(
-        padding: EdgeInsets.all(16),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
         child: SingleChildScrollView(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              CircleAvatar(
+              //Widget para el avatar o foto de contacto
+              const CircleAvatar(
                 backgroundColor: Color.fromARGB(255, 244, 54, 133),
                 radius: 50,
-                child: Text("P",
-                    style: TextStyle(fontSize: 50, color: Colors.white)),
+                child: Text(
+                  "P",
+                  style: TextStyle(fontSize: 50, color: Colors.white),
+                ),
               ),
-              SizedBox(height: 40),
-              Text(
+              const SizedBox(height: 40),
+              
+              //Text del nombre del contacto
+              const Text(
                 "Pascualillo",
                 style: TextStyle(fontSize: 24),
               ),
+              const SizedBox(height: 20),
 
-              Row(
+              Column(
+                children: [
+                  
+                  Container(
+                    height: 1, 
+                    width: double.infinity, 
+                    color: Colors.grey[300], 
+                  ),
 
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        children: [
+                          IconButton(
+                            onPressed: () {},
+                            icon: const Icon(Icons.call, color: Color.fromARGB(255, 150, 142, 142)),
+                          ),
+                          const Text('Llamar'),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          IconButton(
+                            onPressed: () {},
+                            icon: const Icon(Icons.message),
+                          ),
+                          const Text('Mensaje de Texto'),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          IconButton(
+                            onPressed: () {},
+                            icon: const Icon(Icons.videocam),
+                          ),
+                          const Text('Video'),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
               ),
-
             ],
           ),
         ),
